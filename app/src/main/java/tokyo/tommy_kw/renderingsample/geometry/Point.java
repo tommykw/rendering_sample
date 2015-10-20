f93835b5950e0aebf6048671640341743f0d046c
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-10-21 08:10:15 +0900

    implement Point

--- app/src/main/java/tokyo/tommy_kw/renderingsample/geometry/Point.java
+++ app/src/main/java/tokyo/tommy_kw/renderingsample/geometry/Point.java
@@ -16,4 +16,25 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public static Point newInstance(double x, double y) {
+        return new Point(x, y);
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    public Point add() {
+        return null;
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double x = p1.getX() - p2.getX();
+        double y = p1.getY() - p2.getY();
+        return (x * x + y * y);
+    }
+
+    public static double distanceSqrt(Point p1, Point p2) {
+        return Math.sqrt(Point.distance(p1, p2));
+    }
 }
