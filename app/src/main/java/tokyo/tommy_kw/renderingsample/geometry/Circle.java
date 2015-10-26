2b8920aacca2ae0a0bd00c55520afa94acce7af4
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-10-27 01:21:58 +0900

    Circle Impl

--- app/src/main/java/tokyo/tommy_kw/renderingsample/geometry/Circle.java
+++ app/src/main/java/tokyo/tommy_kw/renderingsample/geometry/Circle.java
@@ -3,5 +3,61 @@ package tokyo.tommy_kw.renderingsample.geometry;
 /**
  * Created by tommy on 15/10/20.
  */
-public class Circle {
+public class Circle extends Shape {
+    private Point point;
+    private double radius;
+
+    public Circle() {
+        point = new Point();
+        radius = 1.0;
+    }
+
+    public Circle(Point p, double r) {
+        point = p;
+        radius = r;
+    }
+
+    public Circle(double x, double y, double r) {
+        point = new Point();
+        radius = r;
+    }
+
+    public static Circle newInstace(Point p, double r) {
+        return new Circle(p, r);
+    }
+
+    public Point point() {
+        return point;
+    }
+
+    public double radius() {
+        return radius;
+    }
+
+    public double r() {
+        return radius;
+    }
+
+    public double x() {
+        return point.getX();
+    }
+
+    public double y() {
+        return point.getY();
+    }
+
+    public Point point(double angle) {
+        return point.add(new Vector(Math.cos(angle * (2.0 * Math.PI)), Math.sin(angle * (2.0 * Math.PI))).scale(angle));
+    }
+
+    public Circle scale(double ratio) {
+        return Circle.newInstace(point(), ratio);
+    }
+
+    @Override
+    public Box box() {
+        return null;
+    }
+
+
 }
