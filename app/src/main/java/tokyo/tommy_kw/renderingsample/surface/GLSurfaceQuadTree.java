3aaae75c949d388024ebe3d3f3e195b1c52321c2
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-11-06 00:58:30 +0900

    Added GLSurfaceCircle

---
+++ app/src/main/java/tokyo/tommy_kw/renderingsample/surface/GLSurfaceQuadTree.java
@@ -0,0 +1,7 @@
+package tokyo.tommy_kw.renderingsample.surface;
+
+/**
+ * Created by tommy on 15/11/05.
+ */
+public class GLSurfaceQuadTree {
+}
---
+++ app/src/main/java/tokyo/tommy_kw/renderingsample/surface/GLSurfaceSimpleCircle.java
@@ -0,0 +1,16 @@
+package tokyo.tommy_kw.renderingsample.surface;
+
+import android.content.Context;
+
+import java.util.Random;
+
+/**
+ * Created by tommy on 15/11/05.
+ */
+public class GLSurfaceSimpleCircle extends GLPhotoSurface {
+    protected Random mRandom;
+
+    public GLSurfaceSimpleCircle(Context context) {
+
+    }
+}
